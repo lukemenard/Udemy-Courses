3882d9248013a5812dfd8ac5a5adadb148ca62c5
lukemenard <lukemenard@gmail.com>
2019-12-28 15:09:33 -0700

    completed objects and properties

--- JavaScript/complete-javascript-course/2-JS-basics/starter/script.js
+++ JavaScript/complete-javascript-course/2-JS-basics/starter/script.js
@@ -267,3 +267,29 @@
 
 // var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer'
 // console.log(isDesigner)
+
+
+//OBJECTS AND PROPERTIES
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+}
+console.log(john.firstName)
+console.log(john['lastName'])
+var x = 'birthYear'
+console.log(john[x])
+
+john.job = 'designer'
+john['isMarried'] = true
+console.log(john)
+
+var jane = new Object()
+jane.name = "Jane"
+jane.birthYear = 1969
+jane['lastName'] = 'Smith'
+console.log(jane)
