3f44e56a040ce7f65d079d07aaf431d6bf8d3026
lukemenard <lukemenard@gmail.com>
2020-01-04 14:47:07 -0700

    completed the this keyword in practice

--- JavaScript/complete-javascript-course/3-how-JS-works/starter/script.js
+++ JavaScript/complete-javascript-course/3-how-JS-works/starter/script.js
@@ -36,25 +36,25 @@
 // First scoping example
 
 
-var a = 'Hello!';
-first();
+// var a = 'Hello!';
+// first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
-    function second() {
-        var c = 'Hey!';
-        third()
-        console.log(a + b + c);
-    }
-}
+//     function second() {
+//         var c = 'Hey!';
+//         third()
+//         console.log(a + b + c);
+//     }
+// }
 
-function third(){
-    var d = 'John'
-    // console.log(c)
-    console.log(a+d)
-}
+// function third(){
+//     var d = 'John'
+//     // console.log(c)
+//     console.log(a+d)
+// }
 
 
 
@@ -83,10 +83,37 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//THE THIS KEYWORD IN PRACTICE
 
+// console.log(this)
 
+// calculateAge(1985)
 
+// function calculateAge(year){
+//     console.log(2016 - year)
+//     console.log(this)
+// }
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this)
+        console.log(2016 - this.yearOfBirth)
 
+        // function innerFunction() {
+        //     console.log(this)
+        // }
+        // innerFunction()
+    }
+}
 
+john.calculateAge()
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+}
+
+mike.calculateAge = john.calculateAge
+mike.calculateAge()
